059d5227536aa35662d9b32bb23f1a0311729d6e
Heitor Borazanian <h.borazanian2@gmail.com>
2021-08-16 14:25:31 -0300

    tests: add tests on transaction controller test

--- tests/Feature/TransactionControllerTest.php
+++ tests/Feature/TransactionControllerTest.php
@@ -4,8 +4,12 @@ namespace Tests\Feature;
 
 use App\Enums\TransactionTypesEnum;
 use App\Models\User;
+use App\Observers\TransactionObserver;
+use App\Services\AuthorizationService;
+use App\Services\NotifyService;
 use App\Services\TransactionService;
 use Exception;
+use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 
 class TransactionControllerTest extends TestCase
@@ -79,6 +83,102 @@ class TransactionControllerTest extends TestCase
         $response->assertStatus(400);
         $response->assertJson([
             'message' => 'Usuário não possui saldo suficiente para realizar transação',
+        ]);
+    }
+
+    public function testUsersTransactionCreatingAndCreatedEventSuccess()
+    {
+        $this->partialMock(NotifyService::class, function ($mock) {
+            $mock->shouldReceive('request')->andReturn(
+                (object) ['message' => 'Success']
+            );
+        });
+
+        $this->partialMock(AuthorizationService::class, function ($mock) {
+            $mock->shouldReceive('request')->andReturn(
+                (object) ['message' => 'Autorizado']
+            );
+        });
+
+        Event::fake([TransactionObserver::class]);
+
+        $transactionValue = 100;
+
+        $payer = User::find(1);
+        $payee = User::find(2);
+
+        $payerInitialBalance = (int) $payer->wallet->balance;
+        $payeeInitialBalance = (int) $payee->wallet->balance;
+
+        $transactionData = [
+            'payer' => $payer->id,
+            'payee' => $payee->id,
+            'value' => $transactionValue,
+        ];
+
+        $response = $this->postJson('/api/transaction', $transactionData);
+        $response->assertStatus(200);
+
+        $payer->wallet->refresh();
+        $payee->wallet->refresh();
+
+        $this->assertEquals($payerInitialBalance - $transactionValue, $payer->wallet->balance);
+        $this->assertEquals($payeeInitialBalance + $transactionValue, $payee->wallet->balance);
+    }
+
+    public function testUsersTransactionCreatingEventAuthorizationException()
+    {
+        $this->partialMock(AuthorizationService::class, function ($mock) {
+            $mock->shouldReceive('request')->once()->andReturn(
+                (object) ['message' => 'Error']
+            );
+        });
+
+        Event::fake([TransactionObserver::class]);
+
+        $transactionValue = 100;
+
+        $payer = User::find(1);
+        $payee = User::find(2);
+
+        $transactionData = [
+            'payer' => $payer->id,
+            'payee' => $payee->id,
+            'value' => $transactionValue,
+        ];
+
+        $response = $this->postJson('/api/transaction', $transactionData);
+        $response->assertStatus(400);
+        $response->assertJson([
+            'message' => 'A transação não foi autorizada pelo serviço externo',
+        ]);
+    }
+
+    public function testUsersTransactionCreatedEventNotifyException()
+    {
+        $this->partialMock(NotifyService::class, function ($mock) {
+            $mock->shouldReceive('request')->once()->andReturn(
+                (object) ['message' => 'Error']
+            );
+        });
+
+        Event::fake([TransactionObserver::class]);
+
+        $transactionValue = 100;
+
+        $payer = User::find(1);
+        $payee = User::find(2);
+
+        $transactionData = [
+            'payer' => $payer->id,
+            'payee' => $payee->id,
+            'value' => $transactionValue,
+        ];
+
+        $response = $this->postJson('/api/transaction', $transactionData);
+        $response->assertStatus(500);
+        $response->assertJson([
+            'message' => 'Ocorreu um erro ao tentar enviar notificação',
         ]);
     }
 

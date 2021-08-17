c0734c51b962c89e0d468a2c76a337ebaab93e3d
Heitor Borazanian <h.borazanian2@gmail.com>
2021-08-17 01:19:38 -0300

    feat: add and configure redis

--- config/database.php
+++ config/database.php
@@ -123,7 +123,6 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
         'default' => [
@@ -132,6 +131,7 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
+            'prefix' => 'd:',
         ],
 
         'cache' => [
@@ -140,6 +140,16 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
+            'prefix' => 'c:',
+        ],
+
+        'queue' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_DB', '0'),
+            'prefix' => 'q:',
         ],
 
     ],
